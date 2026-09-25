fdf5512b0da84f07a2af7c067d80a4a564f4caa7
Khushii-0820 <khushivishnoi005@gmail.com>
2026-09-25 16:12:44 +0530

    Create AppTest.java

---
+++ src/test/java/com/devops/AppTest.java
@@ -0,0 +1,17 @@
+package com.devops;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testAddition() {
+        App app = new App();
+
+        int result = app.add(2, 3);
+
+        assertEquals(5, result);
+    }
+}
