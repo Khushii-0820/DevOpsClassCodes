9f214b1f218cb72b50f01097aba8302d0470aae5
Khushii-0820 <khushivishnoi005@gmail.com>
2026-09-25 15:30:13 +0530

    Create App.java

---
+++ src/main/java/com/devops/App.java
@@ -0,0 +1,12 @@
+package com.devops;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("DevOps Maven Project Running Successfully");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
